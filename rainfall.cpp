01410c22926262eff156742c03911b19f236e661
Michael L. Collard <collard@uakron.edu>
2022-01-13 13:03:37 -0500

    Remove using namespace std

--- rainfall.cpp
+++ rainfall.cpp
@@ -9,29 +9,27 @@
 #include <iomanip>
 #include <vector>
 
-using namespace std;
-
 int main() {
-    vector<float> rainfall;
+    std::vector<float> rainfall;
     float t;
     float m;
     float n;
-    while (cin >> n) {
+    while (std::cin >> n) {
         rainfall.push_back(n);
     }
     if (!rainfall.size()) {
-        cout << "Error: no rainfall data";
+        std::cout << "Error: no rainfall data";
         return 1;
     }
     t = rainfall[0];
     m = rainfall[0];
-    for (vector<float>::size_type i = 1; i < rainfall.size(); ++i) {
+    for (std::vector<float>::size_type i = 1; i < rainfall.size(); ++i) {
         t += rainfall[i];
         if (rainfall[i] > m)
             m = rainfall[i];
     }
-    cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << (t / rainfall.size()) << " hundreds of inches" << '\n';
-    cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n';
+    std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << (t / rainfall.size()) << " hundreds of inches" << '\n';
+    std::cout << "Heaviest Hourly Rainfall: " << m << " hundreds of inches" << '\n';
 
     return 0;
 }
