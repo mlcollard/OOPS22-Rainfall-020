e2e8a10db9398e44028ecfc1580f68bb243266f1
Michael L. Collard <collard@uakron.edu>
2022-01-25 13:14:13 -0500

    Inline function avg()

--- rainfall.cpp
+++ rainfall.cpp
@@ -12,12 +12,6 @@
 #include <algorithm>
 #include <numeric>
 
-// average of the data
-double avg(const std::vector<double>& data) {
-
-    return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
-}
-
 // maximum of the data
 double max(const std::vector<double>& data) {
 
@@ -34,8 +28,11 @@ int main() {
         return 1;
     }
 
+    // calculate the average rainfall
+    const auto average = std::accumulate(rainfall.begin(), rainfall.end(), 0.0) / rainfall.size();
+
     // output the rainfall report
-    std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << avg(rainfall) << " hundreds of inches" << '\n';
+    std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << average << " hundreds of inches" << '\n';
     std::cout << "Heaviest Hourly Rainfall: " << max(rainfall) << " hundreds of inches" << '\n';
 
     return 0;
