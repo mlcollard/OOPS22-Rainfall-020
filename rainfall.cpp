9ec7348b0ac064cd8fdbdff81142c795f2fe9d60
Michael L. Collard <collard@uakron.edu>
2022-01-25 13:00:52 -0500

    Extract function max()

--- rainfall.cpp
+++ rainfall.cpp
@@ -21,6 +21,18 @@ double avg(const std::vector<double>& data) {
     return total / data.size();
 }
 
+// maximum of the data
+double max(const std::vector<double>& data) {
+
+    auto maximum = data[0];
+    for (const auto item : data) {
+        if (item > maximum)
+            maximum = item;
+    }
+
+    return maximum;
+}
+
 int main() {
 
     // input hourly rainfall data
@@ -31,16 +43,9 @@ int main() {
         return 1;
     }
 
-    // calculate heaviest rainfall
-    auto max = rainfall[0];
-    for (const auto rain : rainfall) {
-        if (rain > max)
-            max = rain;
-    }
-
     // output the rainfall report
     std::cout << "Average Hourly Rainfall: " << std::fixed << std::setprecision(2) << avg(rainfall) << " hundreds of inches" << '\n';
-    std::cout << "Heaviest Hourly Rainfall: " << max << " hundreds of inches" << '\n';
+    std::cout << "Heaviest Hourly Rainfall: " << max(rainfall) << " hundreds of inches" << '\n';
 
     return 0;
 }
